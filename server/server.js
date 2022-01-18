b8f0bb5fb6cf9e03dede4742115081b5052cec29
heyhmin <74590675+heyhmin@users.noreply.github.com>
2022-01-18 11:48:54 +0900

    betesthm first - module
    FE는 아직 수정하지 않아서 이전에 사용하던 react에서만 테스트하였습니다. module 사용

--- server/server.js
+++ server/server.js
@@ -1,25 +1,32 @@
-const express = require('express');
+import userdb from "./config/userDatabase.js";
+import router from "./routes/index.js"
+import dotenv from "dotenv";
+import express from "express";
 const app = express();
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+import cookieParser from "cookie-parser";
+import bodyParse from "body-parser";
+import cors from "cors";
+
+dotenv.config();
 
 // express 설정
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
-app.use(bodyParser.json());
+app.use(cors({ credentials:true, origin:'http://localhost:3000' }));
+// app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(router);
 
 app.get('/',(req,res) => {
     res.send('Root');
 });
 
+/*
 //mysql connection
-const mysql = require('mysql');
-const dbconfig = require('../server/config/database'); // 데이터베이스 설정파일 경로
+import mysql from "mysql";
+import dbconfig from "./config/database"; // 데이터베이스 설정파일 경로
 const connection = mysql.createPool(dbconfig);
-
+*/
 // 방법 1. Server 단에서 모든 쿼리를 기재하고 수행한다.
 
 // 예시 - 하나의 URL(/test) 당 하나의 쿼리 처리
@@ -46,7 +53,20 @@ app.post('/connect_db', (req, res) => {
                 res.send(rows);
             }
         });
-    });
+});
+
+// user DB 접속 체크
+const userauthdb = async function(){
+  try {
+      await userdb.authenticate();
+      console.log('user Database Connected...');
+
+  } catch (error) {
+      console.error(error);
+  }
+}
+userauthdb();
+
 
 
 // 30020 서버 포트
