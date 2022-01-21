9b7a8dca8c3af106303311d4d3d7d62052eaaa9e
heyhmin <74590675+heyhmin@users.noreply.github.com>
2022-01-21 16:19:56 +0900

    change ignore - database

---
+++ server/config/database.js
@@ -0,0 +1,7 @@
+module.exports = {
+  host : "goormy-database-1.ce7mkzss15bi.ap-northeast-2.rds.amazonaws.com",
+  user : "admin",
+  password : "groomy1!",
+  database : "goormy_first_database",
+  port : 3306
+};
